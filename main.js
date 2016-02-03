bac7ab4301995c5e96370320e216e457a6fb77a9
Huy (Henry) Tran <huytran@appformix.com>
2016-02-03 13:15:02 -0800

    Fix Cmd+V Cmd+C

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const electron = require('electron');
+var Menu = require("menu");
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
@@ -20,4 +21,26 @@ app.on('ready', function() {
   mainWindow.on('closed', function() {
     mainWindow = null;
   });
+
+  // Create the Application's main menu
+    var template = [{
+        label: "Markto",
+        submenu: [
+            { label: "About Markto", selector: "orderFrontStandardAboutPanel:" },
+            { type: "separator" },
+            { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+        ]}, {
+        label: "Edit",
+        submenu: [
+            { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+            { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+            { type: "separator" },
+            { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+            { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+            { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+            { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+        ]}
+    ];
+
+    Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 });
